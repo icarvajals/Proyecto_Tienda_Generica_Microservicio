f245977f02197323d82acd1f36e7b1481900c576
icarvajals <116378462+icarvajals@users.noreply.github.com>
2026-05-23 15:18:48 -0500

    Update ProveedorClient.java

--- ms-productos/ms-productos/src/main/java/tienda_distribuida/ms_productos/unbosque/producto/Controller/ProveedorClient.java
+++ ms-productos/ms-productos/src/main/java/tienda_distribuida/ms_productos/unbosque/producto/Controller/ProveedorClient.java
@@ -6,7 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "MS-PROVEEDORES")
+@FeignClient(
+    name = "ms-proveedores",
+    url = "https://ms-proveedores-production.up.railway.app"
+)
 public interface ProveedorClient {
 
     @GetMapping("/proveedores/buscar/{nit}")
