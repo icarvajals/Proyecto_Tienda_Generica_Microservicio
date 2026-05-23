045814f59671ffd5c7355d25df28baa98393403b
icarvajals <116378462+icarvajals@users.noreply.github.com>
2026-05-23 16:02:41 -0500

    Update ClientesClient.java

--- ms-ventas/ms-ventas/src/main/java/tienda_distribuida/ms_ventas/unbosque/venta/Controller/ClientesClient.java
+++ ms-ventas/ms-ventas/src/main/java/tienda_distribuida/ms_ventas/unbosque/venta/Controller/ClientesClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
     value = "clientes-service",
-    url = "ms-clientes-production.up.railway.app"
+    url = "https://ms-clientes-production.up.railway.app"
 )
 public interface ClientesClient {
 
