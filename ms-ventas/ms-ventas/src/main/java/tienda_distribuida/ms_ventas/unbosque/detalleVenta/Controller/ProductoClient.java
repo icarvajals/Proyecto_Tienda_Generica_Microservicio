1ebe74ec1561a03c5ba04cbacce25c0976706345
icarvajals <116378462+icarvajals@users.noreply.github.com>
2026-05-23 15:45:46 -0500

    Update ProductoClient.java

--- ms-ventas/ms-ventas/src/main/java/tienda_distribuida/ms_ventas/unbosque/detalleVenta/Controller/ProductoClient.java
+++ ms-ventas/ms-ventas/src/main/java/tienda_distribuida/ms_ventas/unbosque/detalleVenta/Controller/ProductoClient.java
@@ -5,7 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "MS-PRODUCTOS")
+@FeignClient(
+    value = "producto-service",
+    url = "ms-productos-production.up.railway.app"
+)
 public interface ProductoClient {
 
     @GetMapping("/api/productos/buscar/{id}")
