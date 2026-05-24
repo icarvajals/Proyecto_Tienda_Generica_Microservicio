e85766651de41b72e4dc869e7d3ae3f2cde90562
icarvajals <116378462+icarvajals@users.noreply.github.com>
2026-05-23 20:04:09 -0500

    cors part 6

--- ms-usuarios/ms-usuarios/src/main/java/tienda_distribuida/ms_usuarios/unbosque/usuario/config/CorsConfig.java
+++ ms-usuarios/ms-usuarios/src/main/java/tienda_distribuida/ms_usuarios/unbosque/usuario/config/CorsConfig.java
@@ -1,19 +1,32 @@
 package tienda_distribuida.ms_usuarios.unbosque.usuario.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Bean
+    public CorsFilter corsFilter() {
 
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://frontend-tienda-production.up.railway.app");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
